3b286f053fb5f8facd1cd6ebfddb441c0e90e7ed
LordNash <thegamebacha@gmail.com>
2016-06-30 23:58:49 +0100

    this is the first commit

---
+++ app/src/main/java/com/google/example/gms/ads/nativeexpressexample/MainActivity.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2016 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.example.gms.ads.nativeexpressexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAdView;
+
+import java.util.List;
+
+
+/**
+ * A simple activity showing the use of a {@link NativeExpressAdView}.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AdLoader.Builder builder = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/3986624511");
+
+        // Add App Install Advertise
+        builder.forAppInstallAd(new NativeAppInstallAd.OnAppInstallAdLoadedListener() {
+            @Override
+            public void onAppInstallAdLoaded(NativeAppInstallAd ad) {
+                FrameLayout frameLayout =
+                        (FrameLayout) findViewById(R.id.fl_adplaceholder);
+                NativeAppInstallAdView adView = (NativeAppInstallAdView) getLayoutInflater()
+                        .inflate(R.layout.ad_app_install, null);
+                populateAppInstallAdView(ad, adView);
+                frameLayout.removeAllViews();
+                frameLayout.addView(adView);
+            }
+        });
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(MainActivity.this, "Failed to load native ad: "
+                        + errorCode, Toast.LENGTH_SHORT).show();
+            }
+        }).build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+
+    }
+
+    private void populateAppInstallAdView(NativeAppInstallAd nativeAppInstallAd,
+                                          NativeAppInstallAdView adView) {
+
+
+        adView.setHeadlineView(adView.findViewById(R.id.appinstall_headline));
+        adView.setImageView(adView.findViewById(R.id.appinstall_image));
+        adView.setBodyView(adView.findViewById(R.id.appinstall_body));
+        adView.setCallToActionView(adView.findViewById(R.id.appinstall_call_to_action));
+        adView.setIconView(adView.findViewById(R.id.appinstall_app_icon));
+        adView.setPriceView(adView.findViewById(R.id.appinstall_price));
+        adView.setStarRatingView(adView.findViewById(R.id.appinstall_stars));
+        adView.setStoreView(adView.findViewById(R.id.appinstall_store));
+
+        // Some assets are guaranteed to be in every NativeAppInstallAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAppInstallAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeAppInstallAd.getBody());
+        ((Button) adView.getCallToActionView()).setText(nativeAppInstallAd.getCallToAction());
+        ((ImageView) adView.getIconView()).setImageDrawable(nativeAppInstallAd.getIcon()
+                .getDrawable());
+
+        List<NativeAd.Image> images = nativeAppInstallAd.getImages();
+
+        if (images.size() > 0) {
+            ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+        }
+
+        // Some aren't guaranteed, however, and should be checked.
+        if (nativeAppInstallAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAppInstallAd.getPrice());
+        }
+
+        if (nativeAppInstallAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAppInstallAd.getStore());
+        }
+
+        if (nativeAppInstallAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAppInstallAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAppInstallAd);
+    }
+
+
+}
